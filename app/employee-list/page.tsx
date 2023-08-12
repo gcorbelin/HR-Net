66f29ef9a3dd2860128091401e6870ab32794b12
CORBELIN Guillaume <guillaume.corbelin@coexya.eu>
2023-08-12 13:43:04 +0200

    ✨ Add search for data table

--- app/employee-list/page.tsx
+++ app/employee-list/page.tsx
@@ -15,7 +15,15 @@ export const metadata: Metadata = {
 export default function EmployeeList() {
   const employeesStore = useAppSelector(selectEmployees);
   const [isLoading, setIsLoading] = useState(true);
-  let employeesDatas: Employee[] = employeesStore.employees;
+  const [filterText, setFilterText] = useState("");
+  // Get datas from store
+  const employeesDatas: Employee[] = employeesStore.employees;
+  // Filter datas with search input
+  const filteredItems = employeesDatas.filter(
+    (item) =>
+      JSON.stringify(item).toLowerCase().indexOf(filterText.toLowerCase()) !==
+      -1
+  );
 
   useEffect(() => {
     setIsLoading(false);
@@ -88,15 +96,44 @@ export default function EmployeeList() {
     },
   ];
 
+  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterText(event.currentTarget.value);
+  };
+
+  const handleClear = () => {
+    if (filterText) {
+      setFilterText("");
+    }
+  };
+
   return (
     <main className="p-6">
       <div className="container mx-auto">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold mb-2">Current Employees</h1>
         </div>
+        <div className="flex justify-end mb-2">
+          <div className="relative rounded-md shadow-sm">
+            <input
+              id="search"
+              type="text"
+              className="input pr-4 m-0"
+              placeholder="Filter table data..."
+              value={filterText}
+              onChange={(event) => handleFilter(event)}
+            />
+            <button
+              type="button"
+              onClick={handleClear}
+              className="absolute inset-y-0 right-0 flex items-center rounded-r-md bg-gray-100 px-3 h-100 border border-gray-300"
+            >
+              X
+            </button>
+          </div>
+        </div>
         <DataTable
           columns={columns}
-          data={employeesDatas}
+          data={filteredItems}
           pagination
           progressPending={isLoading}
           className="mt-2"
